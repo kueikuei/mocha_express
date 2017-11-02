84ee43b86611273e79ff7e1d6e1376d9db75e77c
KueiKuei <r03228001@ntu.edu.tw>
2017-11-02 12:39:40 +0800

    Mongo Basic

---
+++ n-7-6-objectid/playground/mongodb-connect.js
@@ -0,0 +1,47 @@
+const MongoClient = require('mongodb').MongoClient;
+// require('./ES6.js');
+// const {MongoClient, ObjectID} = require('mongodb');
+
+// const Mongo = require('mongodb');
+
+// var obj = Mongo.ObjectID();
+// var obj = new ObjectID();
+// console.log(obj);
+
+// const obj = {name:'Jason',age:23};
+// const {name} = obj;
+// console.log(name);
+
+
+MongoClient.connect('mongodb://localhost:27017/todo', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').insertOne({
+    text: 'Insert test2',
+    completed: true
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+  
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  db.close();
+});
---
+++ n-7-7-fetching-data/playground/mongodb-find.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+// const {MongoClient, ObjectID} = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
+
+MongoClient.connect('mongodb://localhost:27017/todoapp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  //test 1.
+  db.collection('todos').find({
+    // "completed" : false
+    _id: new ObjectID('586b4132d1d01f38cf0da8ea')
+    // "_id" : new ObjectId("586b4132d1d01f38cf0da8ea") //ObjectId is not defined
+  }).toArray().then((docs) => {
+    //if not toArray()
+    //TypeError: db.collection(...).find(...).then is not a function
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  //test 2.
+  // db.collection('todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  //test 3.
+  // db.collection('todos').find({name: 'Andrew'}).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // });
+
+  // db.close();
+});
